095fd7e039a6a3878cd2fa17b41a7a8b575a6434
ShofyKanisa <shofy0606karimatunnisa@gmail.com>
2024-02-19 10:58:44 +0700

    menambah script.js untuk button alert

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    swal({
+      title: "Kamu belum register!",
+      text: "Register terlebih dahulu",
+      icon: "warning",
+      buttons: ['Oh Naur!', 'Aw Yiss!']
+    });
+  }
